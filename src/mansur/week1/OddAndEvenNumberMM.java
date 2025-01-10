7d659946cc16d0b75f1cc0f360e15c134bfa17ac
Mansurmohamed <abdullamansur3@gmail.com>
2025-01-10 17:08:24 -0500

    my first commit to the group!

--- src/mansur/week1/OddAndEvenNumberMM.java
+++ src/mansur/week1/OddAndEvenNumberMM.java
@@ -5,5 +5,19 @@ public class OddAndEvenNumberMM {
     public static void main(String[] args) {
 
         System.out.println("Hello team");
+
+
+        String result = identify(2);
+        String result2 = identify(3);
+        System.out.println(result);
+        System.out.println(result2);
+
+    }
+
+    public static String identify(int num){
+        if (num % 2 != 0){
+            return "odd";
+        }
+        return "even";
     }
 }
