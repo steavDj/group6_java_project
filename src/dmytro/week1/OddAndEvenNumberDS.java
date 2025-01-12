63311bf916adc10f7ed075b688093f9c950c1990
David <sw.dmitrij@gmail.com>
2025-01-12 11:59:03 -0500

    project updated added identify method which identify is number even or odd

--- src/dmytro/week1/OddAndEvenNumberDS.java
+++ src/dmytro/week1/OddAndEvenNumberDS.java
@@ -6,6 +6,20 @@ public class OddAndEvenNumberDS {
 
         System.out.println("Hello Best Team Ever!!");
 
+        identify(5);
+
+        identify(22);
+
+    }
+
+    public static void identify(int num) {
+
+        // Check if number has a remainder after dividing and print out result
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+
     }
 
 }
