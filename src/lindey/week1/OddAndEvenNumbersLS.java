10bc2bde00bfd60cd6a591abe471669518ed7cf0
lindeysmith <lindlaine@gmail.com>
2025-01-17 15:39:00 -0600

    lindey-week 1 update

---
+++ src/lindey/week1/OddAndEvenNumbersLS.java
@@ -0,0 +1,22 @@
+package lindey.week1;
+
+public class OddAndEvenNumbersLS {
+    public static void main(String[] args) {
+        OddOrEven(5);
+        OddOrEven(6);
+    }
+
+
+    public static void OddOrEven (int num){
+        if (num % 2 == 1){
+            System.out.println("Number is odd");;
+        }else System.out.println("Number is even");;
+    }
+}
+/*
+    Numbers -- Odd & Even
+    Write a method that identifies whether a given number is even or odd
+    EX:
+    identify(5) -> "Odd"
+    identify(6) -> "Even"
+ */
