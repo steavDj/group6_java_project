4d3a31f0f7ce32bc0cee583dcebb721c241d9103
NeonBlueSky <sanatarat@outlook.com>
2025-01-17 13:10:41 -0600

    Test

---
+++ src/sanat/week01/OddAndEvenNumber2S.java
@@ -0,0 +1,34 @@
+package sanat.week01;
+
+import java.util.Scanner;
+
+public class OddAndEvenNumber2S {
+    public static void main(String[] args) {
+
+        System.out.println("Week 1 Odd or Even Project Test");
+
+        Scanner scanner = new Scanner(System.in);
+        int Number = 0;
+        boolean ValidInput = false;
+
+        while (!ValidInput) {
+
+            System.out.print("Enter a Number: ");
+            if (scanner.hasNextInt()) {
+                Number = scanner.nextInt();
+                ValidInput = true;
+            } else {
+                System.out.println("Invalid input. Please enter a valid Number.");
+                scanner.next();
+            }
+        }
+
+        if (Number % 2 == 0) {
+            System.out.println(Number + " is even.");
+        } else {
+            System.out.println(Number + " is odd.");
+        }
+
+        scanner.close();
+    }
+}
