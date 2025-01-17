510f9211818dab052eec5dd154204e40efeacd38
NeonBlueSky <sanatarat@outlook.com>
2025-01-17 13:18:39 -0600

    Test

--- src/anna/week1/OddAndEvenNumberAN.java
+++
@@ -1,7 +0,0 @@
-package anna.week1;
-
-public class OddAndEvenNumberAN {
-    public static void main(String[] args) {
-        System.out.println("Hello Guys!");
-    }
-}
--- src/oleksandr/week01/OddAndEvenNumberOB.java
+++
@@ -1,7 +0,0 @@
-package oleksandr.week01;
-
-public class OddAndEvenNumberOB {
-    public static void main(String[] args) {
-        System.out.println("Hello team and let's go!");
-    }
-}
