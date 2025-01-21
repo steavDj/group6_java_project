00dd192f7a4333818c9d09706bb66b2b998d856f
David <sw.dmitrij@gmail.com>
2025-01-20 19:05:42 -0500

    FINRA task is completed

--- src/dmytro/week2/FINRA_DS.java
+++ src/dmytro/week2/FINRA_DS.java
@@ -1,6 +1,6 @@
 package dmytro.week2;
 
-public class FINRA {
+public class FINRA_DS {
 
     public static void main(String[] args) {
 
@@ -20,11 +20,12 @@ public class FINRA {
     }
 
 }
+
 /*
-Next week Java Coding Tasks - Week2
+Java Coding Tasks - Week2
 FINRA:
-Write a function that prints out the numbers from 1 to 30
-but for numbers that are a multiple of 3, print "FIN" instead of the number,
-and for numbers that are a multiple of 5, print "RA" instead of the number.
+Write a function that prints out the numbers from 1 to 30 but
+for numbers that are a multiple of 3, print "FIN" instead of the number, and
+for numbers that are a multiple of 5, print "RA" instead of the number.
 For numbers that are a multiple of both 3 and 5, print "FINRA" instead of the number.
  */
