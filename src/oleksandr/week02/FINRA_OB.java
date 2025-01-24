e4deb4a9a137fa2487866cfe80d7d5bbb8876aa5
bleio <blei.oleksand@gmail.com>
2025-01-24 12:37:47 -0500

    The second task "FINRA" is done

---
+++ src/oleksandr/week02/FINRA_OB.java
@@ -0,0 +1,26 @@
+package oleksandr.week02;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class FINRA_OB {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+       System.out.println("Please input your number: ");
+
+        int num = scanner.nextInt();
+        for (int i = 1; i <= num; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("FINRA ");
+            } else if (i % 3 == 0) {
+                System.out.print("FIN ");
+            } else if (i % 5 == 0) {
+                System.out.print("RA ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
