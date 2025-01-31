5e56f8e2d22e1c06796c210566cc626115348b78
Xander <Xenitharino@gmail.com>
2025-01-31 12:03:20 -0600

    Project Fix

---
+++ src/dmytro/week1/OddAndEvenNumberDS.java
@@ -0,0 +1,27 @@
+package dmytro.week1;
+
+public class OddAndEvenNumberDS {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Best Team Ever!!");
+
+        identify(5);
+
+        identify(22);
+
+        identify(56);
+
+    }
+
+    public static void identify(int num) {
+
+        // Checking if number has a remainder after dividing and print out result.
+        if (num % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+
+    }
+
+}
