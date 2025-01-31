e2c92f2711e8c00fa07a73eef7b172e6bddf7908
Xander <Xenitharino@gmail.com>
2025-01-31 11:26:18 -0600

    SwapNumber Task 3 Completed
    Additional Test Cases with a method included

---
+++ src/xander/week3/SwapNumbersXS.java
@@ -0,0 +1,31 @@
+package xander.week3;
+
+public class SwapNumbersXS {
+    public static void main(String[] args) {
+
+        // Additional Test Cases
+        numberSwapper(5, 10);
+        System.out.println("---");
+        numberSwapper(14, 56);
+        System.out.println("---");
+        numberSwapper(75, 90);
+
+    }
+
+    public static void numberSwapper(int num1, int num2) {
+
+        System.out.println("Before the swap");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        num1 = num1 + num2;
+        num2 = num1 - num2;
+        num1 = num1 - num2;
+
+        System.out.println("After the swap");
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+    }
+
+}
