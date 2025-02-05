8b4a7e44a5dad8368fffdcafeacd43260c30357d
Mansurmohamed <abdullamansur3@gmail.com>
2025-02-04 20:18:31 -0500

    SwapNumbers task done

---
+++ src/mansur/week3/SwapNumbers_MM.java
@@ -0,0 +1,19 @@
+package mansur.week3;
+
+public class SwapNumbers_MM {
+
+    public static void main(String[] args){
+
+        int a = 7, b =10;
+        int c;
+
+        c = a;
+        a = b;
+        b = c;
+
+        System.out.println(a);
+        System.out.println(b);
+
+
+    }
+}
