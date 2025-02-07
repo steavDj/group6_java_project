2452b7c4486ea33d4246262c758e96a92cf62dae
lindeysmith <lindlaine@gmail.com>
2025-02-06 21:27:02 -0600

    Week4 Java Coding Task

---
+++ src/lindey/week4/ReverseNegativeNums.java
@@ -0,0 +1,16 @@
+package lindey.week4;
+
+public class ReverseNegativeNums {
+    public static void main(String[] args) {
+        System.out.println("reverseNegativeNums(0) = " + reverseNegativeNums(0));
+        System.out.println("reverseNegativeNums(-18) = " + reverseNegativeNums(-18));
+        System.out.println("reverseNegativeNums(24) = " + reverseNegativeNums(24));
+
+    }
+
+    public static int reverseNegativeNums(int x){
+        if (x<0){
+            x = -(x);
+        }return x;
+    }
+}
