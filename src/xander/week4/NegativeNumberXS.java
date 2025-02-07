99731837df948e3fd890675f01e31a20d63512d1
Alexander Whitestone <Xenitharino@gmail.com>
2025-02-06 22:33:49 -0600

    Update NegativeNumberXS.java

--- src/xander/week4/NegativeNumberXS.java
+++ src/xander/week4/NegativeNumberXS.java
@@ -5,7 +5,7 @@ public class NegativeNumberXS {
 
         System.out.println(reverseNum(-10));
         System.out.println(reverseNum(-50));
-        System.out.println(reverseNum(-100));
+        System.out.println(reverseNum(-100123123));
 
     }
 
