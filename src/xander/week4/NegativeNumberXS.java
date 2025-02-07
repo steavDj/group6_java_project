88b35dd2605deb505bda878b6b66d7d2460966ed
Alexander Whitestone <Xenitharino@gmail.com>
2025-02-06 22:30:26 -0600

    Negative Number Task 4 Completed

---
+++ src/xander/week4/NegativeNumberXS.java
@@ -0,0 +1,17 @@
+package xander.week4;
+
+public class NegativeNumberXS {
+    public static void main(String[] args) {
+
+        System.out.println(reverseNum(-10));
+        System.out.println(reverseNum(-50));
+        System.out.println(reverseNum(-100));
+
+    }
+
+    public static int reverseNum(int num) {
+        return -num;
+
+    }
+
+}
