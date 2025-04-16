2cd2b9582106c896dbd61eb8233fa0fd9e4536dc
irynas11 <soroka.v.iryna@gmail.com>
2025-04-16 18:18:59 -0500

    Task 12 Done

--- src/iryna/week11/SortArrayNoSortingMethod.java
+++ src/iryna/week11/SortArrayNoSortingMethod.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 public class SortArrayNoSortingMethod {
     public static void main(String[] args) {
         int[] arr = {10, 9, 8, 7};
+        int[] arr1 = {11, 34, 2, 44,55,7};
+
         sortingArrayBubble(arr);
+        sortingArrayBubble(arr1);
 
         sortingArrayBubbleBoolean(arr);
 
---
+++ src/iryna/week12/ArraySumIsZero.java
@@ -0,0 +1,30 @@
+package iryna.week12;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ArraySumIsZero {
+    public static void main(String[] args) {
+        System.out.println("(sumIsZero(3)) = " + Arrays.toString(sumIsZero(3)));
+        System.out.println("(sumIsZero(4)) = " + Arrays.toString(sumIsZero(4)));
+        System.out.println("(sumIsZero(7)) = " + Arrays.toString(sumIsZero(7)));
+    }
+
+    public static int[] sumIsZero(int num) {
+
+        int[] numbers = new int[num];
+        Random random = new Random();
+        int sum = 0;
+
+        for (int i = 0; i < numbers.length - 1; i++) {
+            // numbers[i] = random.nextInt(20) -40;
+            numbers[i] = random.nextInt(num * 2 + 1) - num;
+            numbers[i] = random.nextInt(20) - 40;
+            sum += numbers[i];
+        }
+
+        numbers[num - 1] = -sum;
+
+        return numbers;
+    }
+}
